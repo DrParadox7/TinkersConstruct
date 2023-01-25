06a21a30e60b8221007f28243791dbd1c4853193
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-01-25 23:49:09 +0100

    quick fix : Multiple occurrences of the same expression

--- src/main/java/tconstruct/tools/gui/BattlesignGui.java
+++ src/main/java/tconstruct/tools/gui/BattlesignGui.java
@@ -78,19 +78,23 @@ public class BattlesignGui extends GuiScreen {
 
         float lum = calcLuminance(bgColR, bgColG, bgColB);
         for (int i = 0; i < text.length; i++) {
+            final EnumChatFormatting chatFormatting = lum >= 35F
+                    ? EnumChatFormatting.BLACK
+                    : lum >= 31F ? EnumChatFormatting.GRAY : EnumChatFormatting.WHITE;
             fontRendererObj.drawString(
-                    (lum >= 35F ? EnumChatFormatting.BLACK
-                            : lum >= 31F ? EnumChatFormatting.GRAY : EnumChatFormatting.WHITE)
+                    chatFormatting
                             + (i == currentLine ? "> " : "")
                             + text[i]
                             + (i == currentLine
-                                    ? " \u00A7r"
-                                            + (lum >= 35F ? EnumChatFormatting.BLACK
-                                                    : lum >= 31F ? EnumChatFormatting.GRAY : EnumChatFormatting.WHITE)
-                                            + "<"
-                                    : ""),
-                    k - fontRendererObj.getStringWidth(
-                            (i == currentLine ? "> " : "") + text[i] + (i == currentLine ? " <" : "")) / 2 + 51,
+                            ? " " + EnumChatFormatting.RESET
+                            + chatFormatting
+                            + "<"
+                            : ""),
+                    k
+                            - fontRendererObj.getStringWidth(
+                                            (i == currentLine ? "> " : "") + text[i] + (i == currentLine ? " <" : ""))
+                                    / 2
+                            + 51,
                     l + 4 + 10 * i,
                     0);
         }
